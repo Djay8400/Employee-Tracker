e32f327099c91f1325a146e1f4d81f47a8a7f94c
David Jay <djay8400@gmail.com>
2021-10-19 20:14:55 -0500

    finished functionality of update employee role

--- server.js
+++ server.js
@@ -96,10 +96,10 @@ const addEmployee = () => {
         },
      ])
      .then((response) => {
+        // use response to create a new employee on Employee Table
         let empRoleId = response.role.split(' ').shift();
         let empManagerId = response.manager.split(' ').shift();
 
-        // use response to create a new employee on Employee Table
         let empColumns = `(first_name, last_name, role_id, manager_id)`
         let employees = `VALUES 
             ("${response.first}", "${response.last}", ${empRoleId}, ${empManagerId})`
@@ -116,38 +116,46 @@ const addEmployee = () => {
  }) 
 }
 
+const updateEmpRole = () => {  
+    db.query("SELECT id, title FROM roles", function (err, results) {
+        const empTitles2 = results.map(role => {
+            return `${role.id} ${role.title}`;
+        });
+    db.query("SELECT id, first_name, last_name FROM employee", function (err, results) {
+        const empNames2 = results.map(names => {           
+            return `${names.id} ${names.first_name} ${names.last_name}`;
+        });         
+    inquirer 
+      .prompt([
+        {
+        type: "list",
+        name: "employee",
+        message: "Which employee's role do you want to update?",
+        choices: empNames2, //populate here what employees there are from Employee Table   
+        },
+        {
+        type: "list",
+        name: "role",
+        message: "Which role do you want to assign the selected employee?",
+        choices: empTitles2, //needs a way to populate here what the roles are from Role Table 
+        },
+     ])
+     .then((response) => {
+        // use response to change employee's role on Role Table
+        let empRoleId2 = response.employee.split(' ').shift();
+        let empManagerId = response.role.split(' ').shift();
+
+        db.query(`UPDATE employee SET role_id = ${empManagerId} WHERE id = ${empRoleId2}`, (err, results) => {
+            console.log(results);
+        });
+        
+        userChoice();
+    }) 
+  })                          
+})                           
+}
+
 userChoice();
-
-// const updateEmpRole = () => {  
-//     inquirer 
-//       .prompt([
-//         {
-//         type: "list",
-//         name: "employee",
-//         message: "Which employee's role do you want to update?",
-//         choices:[""], //needs a way to populate here what employees there are from Employee Table   
-//         },
-//         {
-//         type: "list",
-//         name: "role",
-//         message: "Which role do you want to assign the selected employee?",
-//         choices:[""], //needs a way to populate here what the roles are from Role Table 
-//         },
-//      ])
-//      .then((response) => {
-//         // use response to change employee's role on Role Table
-
-
-//         // db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-//         //     console.log(results);
-//         // });
-          
-//         // db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-//         //     console.log(results);
-//         // });
-//         userChoice();
-//     })                           
-// }
 
 // const viewRoles = () => { 
 //     // show role table
